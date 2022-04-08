fd80b6aa333da06bf2c6b438fba6c4f2bce864b1
Sergio <sergioandyp@gmail.com>
2022-04-08 18:39:15 -0300

    Calculo de log2 y bireverse

--- FFT/FFT.cpp
+++ FFT/FFT.cpp
@@ -1,10 +1,36 @@
 #include "FFT.hpp"
+
 #include <iostream>
 
 using namespace std;
 
+int _log2(size_t n);
+int bitReverse(int i, int logn);
+
+
 void fft(complex<double> *in, complex<double> *out, size_t n) {
 
-    // Magic happens here    
+    // Caclcular gamma = floor(log2(n))
+    int gamma = _log2(n);
 
+    int r = 0;
+
+}
+
+int _log2(size_t n) {
+    int res = 0;
+    while (n > 1) {
+        n >>= 1;
+        res++;
+    }
+    return res;
+}
+
+int bitReverse(int i, int logn) {
+    int res = 0;
+    for (int k = 0; k < logn; k++) {
+        res = (res << 1) + (i & 1);
+        i >>= 1;
+    }
+    return res;
 }
